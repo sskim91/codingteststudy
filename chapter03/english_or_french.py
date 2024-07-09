5872a875e0992fd6f90a9304ee6f022c392277ce
sskim91 <tjdtjq91@gmail.com>
2024-07-09 18:03:20 +0900

    chapter 3 연습문제

--- chapter03/english_or_french.py
+++ chapter03/english_or_french.py
@@ -1,1 +1,20 @@
 # https://dmoj.ca/problem/ccc11s1
+
+# 입력 받기
+N = int(input())
+
+english_words = 0
+french_words = 0
+
+# N개의 줄을 입력받아 문자 개수 세기
+for _ in range(N):
+    line = input()
+    english_words += line.count('t') + line.count('T')
+    french_words += line.count('s') + line.count('S')
+
+# 결과 출력
+if english_words > french_words:
+    print("English")
+else:
+    print("French")
+
--- chapter03/multiple_choice.py
+++ chapter03/multiple_choice.py
@@ -1,1 +1,20 @@
 # https://dmoj.ca/problem/ccc11s2
+
+N = int(input())
+
+student_answer = ''
+correct_answer = ''
+
+for i in range(N):
+    student_answer += input()
+
+for i in range(N):
+    correct_answer += input()
+
+answer = 0  # 맞은 개수 초기화
+
+for i in range(N):
+    if student_answer[i] == correct_answer[i]:
+        answer += 1
+
+print(answer)
